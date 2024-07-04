178ab0248c0862958228639722d6ac98854d9e90
oussama-ayari-2020 <oussema.ayari2001@gmail.com>
2024-07-04 12:37:13 +0100

    +1 Medium Solution

--- src/Custom_Data_Structures/ListNode.java
+++ src/Custom_Data_Structures/ListNode.java
@@ -1,9 +1,9 @@
 package Custom_Data_Structures;
 
 public class ListNode {
-    int val;
+    public int val;
     public ListNode next;
     ListNode() {}
     ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
